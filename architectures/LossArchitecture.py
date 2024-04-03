afd5a5be5e224ef06e7693d2574c7709e30a93b2
meet-sheth-wb <meet.sheth@weboccult.com>
2024-04-03 14:23:56 +0530

    Changed the ssim loss

--- architectures/LossArchitecture.py
+++ architectures/LossArchitecture.py
@@ -8,39 +8,19 @@ class RateLoss(tf.keras.losses.Loss):
         return y_pred
     
 class SSIMLoss(tf.keras.losses.Loss):
-    def __init__(self, window_size=11,strides=[1,1,1,1],padding='SAME',reduction="auto", name=None):
+    def __init__(self,weight=const.LOSS_WEIGHT,alpha=const.ALPHA,reduction="auto", name=None):
         super().__init__(reduction, name)
-        self.weight = const.SSIM_WEIGHT
-        self.window_size=window_size
-        self.strides=strides
-        self.padding=padding
-        self.gaussian_kernel = self.make_gaussian_kernel(
-            self.window_size,
-            const.CHANNELS,
-            sigma=const.SSIM_SIGMA
-        )
-        self.C1 = tf.constant(0.01 ** 2)
-        self.C2 = tf.constant(0.02 ** 2)
-        self.M = tf.constant(2.)
-    
-    def make_gaussian_kernel(self,window_size,channel,mean=0,sigma=1):
-            d = tfp.distributions.Normal(mean,sigma)
-            size = window_size//2
-            val = d.prob(tf.range(start=-size,limit=size+1,delta=1,dtype=tf.float32))
-            kernel_2d = tf.einsum('i,j->ij',
-                                     val,
-                                     val)
-            gauss_kernel = kernel_2d / tf.reduce_mean(kernel_2d)
-            gauss_kernel = gauss_kernel[:,:,tf.newaxis,tf.newaxis]
-            gauss_kernel = tf.tile(gauss_kernel,[1,1,channel,1])
-            
-            return gauss_kernel
+        self.alpha = alpha
+        self.weight = weight
     
     def call(self,y_true,y_pred):
         
-        ssim_score = tf.image.ssim(y_true,y_pred,1.0)
+        ssim_score = 1. - tf.math.reduce_mean(tf.image.ssim(y_true,y_pred,1.0))
+        l1_loss = tf.math.reduce_mean(tf.math.abs(y_true-y_pred))
         
-        return ssim_score
+        final_loss = self.alpha * ssim_score + (1-self.alpha) * l1_loss
+        
+        return self.weight * final_loss
         
         
 
