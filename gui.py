fb2cfb20e20371497d5d55337525e1d00bba863b
Meet Sheth <meetsheth1307@gmail.com>
2024-04-10 02:00:19 +0530

    Completed GUI

--- gui.py
+++ gui.py
@@ -1,27 +1,33 @@
 #import torch.nn as nn
 import cv2
-import torch
+import tensorflow as tf
 import numpy as np
 from tkinter import *
-from PIL import Image,ImageGrab,ImageTk
-from paramtest import Network
+from PIL import Image,ImageDraw,ImageTk
 import matplotlib.pyplot as plt
+from io import BytesIO
 
 
 def model(x):
 
-
-    model1 = torch.load('pytorchmodel.pth')
-    model1.eval()
-    return model1.feedforward(x)
+    global image_number,encoder,decoder
+      
+    
+    compressed = encoder.serve(x)
+    regenerated = decoder.serve(compressed)
+    
+    with open(f"compressed_imgs/{image_number}.cmp",'wb') as f:
+        f.write(compressed.numpy().tobytes())
+        f.close()
+    
+    image_number+=1
+    return regenerated.numpy()[0]*255.
 
 
 def clear_widget():
-    global cv
-    i = Image.open('em.jpg')
-    lbl_image = ImageTk.PhotoImage(i)
-    lbl.configure(image=lbl_image)
-    lbl.image = lbl_image
+    global cv, new_img,draw
+    new_img = Image.new("L",(280,280))
+    draw = ImageDraw.Draw(new_img)
     cv.delete("all")
 
 def activate_event(event):
@@ -30,45 +36,27 @@ def activate_event(event):
     lastx,lasty = event.x,event.y
 
 def draw_lines(event):
-    global lastx,lasty
+    global lastx,lasty,new_img
     x,y = event.x,event.y
     cv.create_line((lastx,lasty,x,y),width=8,fill='white',
                    capstyle=ROUND,smooth=True,splinesteps=12)
+    draw.line((lastx,lasty,x,y),width=8,fill='white')
     lastx,lasty=x,y
 
 def Recognise_Digit():
-    global image_number
-
-    filename = f'image.png'
-    widget = cv
-
-    x=root.winfo_rootx()+widget.winfo_x()
-    y=root.winfo_rooty()+widget.winfo_y()
-    x1=x+widget.winfo_width()
-    y1=y+widget.winfo_height()
-
-    ImageGrab.grab().crop((x,y,x1,y1)).save(filename)
-
-    image = cv2.imread(filename,cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    ret,th = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    contours = cv2.findContours(th,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
-
-    for cnt in contours:
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),1)
-        img = cv2.resize(th,(28,28),interpolation=cv2.INTER_AREA)
-        img = img/255.0
-        x1 = np.ones(img.shape)
-        img = x1-img
-        img = img.reshape(1,1,28,28).astype('float32')
-        img = torch.tensor(img)
-        pred = model(img)
-        final_pred = torch.argmax(pred)
-        f = Image.open("{0}.jpg".format(final_pred))
-        lbl_image = ImageTk.PhotoImage(f)
-        lbl.configure(image=lbl_image)
-        lbl.image = lbl_image
+    global new_img
+    img = np.array(new_img,dtype=np.uint8)
+    interpolation = cv2.INTER_NEAREST
+    img = cv2.resize(img,(28,28),interpolation)
+    regen = model(np.reshape(img,(1,28,28,1))).astype(np.uint8)
+    scale = 10
+    img = img.repeat(scale,axis=1).repeat(scale,axis=0)
+    regen = regen.repeat(scale,axis=1).repeat(scale,axis=0)
+    # img = cv2.resize(img,(212,212),cv2.INTER_LINEAR)
+    cv2.imshow("Image",img)
+    cv2.imshow("rege",regen)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 root = Tk()
@@ -79,17 +67,18 @@ root.title("Handwritten Digit Recognition")
 lastx,lasty = None,None
 image_number = 0
 
+encoder = tf.saved_model.load("exported_models/encoder")
+decoder = tf.saved_model.load("exported_models/decoder")  
+
 cv = Canvas(root,width = 280,height = 280,bg = 'black')
 cv.grid(row=0,column=0,sticky=W,columnspan=1)
 cv.bind('<Button-1>',activate_event)
 
-i = Image.open('em.jpg')
-lbl_image = ImageTk.PhotoImage(i)
-lbl = Label(root,image=lbl_image)
-lbl.grid(row=0,column=1,sticky=NW)
-btn_save = Button(text="Recognise Digit",command=Recognise_Digit,fg='black',bg='deepskyblue',width=19)
+new_img = Image.new("L",(280,280))
+draw = ImageDraw.Draw(new_img)
+btn_save = Button(text="Generate",command=Recognise_Digit,fg='black',bg='deepskyblue',width=19)
 btn_save.grid(row=2,column=0,sticky=W,pady=1)
-btn_clear = Button(text='Clear Widget',command=clear_widget,fg='black',bg='deepskyblue',width=19)
+btn_clear = Button(text='Clear',command=clear_widget,fg='black',bg='deepskyblue',width=19)
 btn_clear.grid(row=2,column=0,sticky=E)
 
 root.mainloop()
